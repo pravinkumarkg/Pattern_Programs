31a03dc395569459a33e152b17d557f963e85146
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-09-08 18:56:54 +0530

    Update hollow_left_triangle.c

--- hollow_left_triangle.c
+++ hollow_left_triangle.c
@@ -5,13 +5,13 @@ int main(void) {
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
 	{
-		for(j=1;j<=n;j++)
+		for(j=i;j<n;j++)
 		{
 			printf(" ");
 		}
 		for(j=1;j<=i;j++)
 		{
-			if(i==1||j==1||i==n||j==n||i==j)
+			if(i==1||i==n||j==1||j==n||i==j)
 			printf("*");
 			else
 			printf(" ");
