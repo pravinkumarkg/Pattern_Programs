f604b36d87f4b68611de04112bbfe61f5f51e1e0
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-09-09 11:57:08 +0530

    Create Heart_With_Name.c

---
+++ Heart_With_Name.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	int i,j,n,len;
+	char name[40];
+	scanf("%s",&name);
+	scanf("%d",&n);
+	len=strlen(name);
+	for(i=n/2;i<=n;i+=2)
+	{
+		for(j=1;j<n-i;j+=2)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(i=n;i>=1;i--)
+	{
+		for(j=i;j<n;j++)
+		{
+			printf(" ");
+		}
+		if(i==n)
+		{
+			for(j=1;j<=(n*2-len)/2;j++)
+			{
+				printf("*");
+			}
+			printf("%s",name);
+			for(j=1;j<(n*2-len)/2;j++)
+			{
+				printf("*");
+			}
+		
+		}
+		else
+		{
+		for(j=1;j<=(i*2)-1;j++)
+		{
+			printf("*");
+		}
+		
+		}
+		printf("\n");
+	}
+	return 0;
+}
